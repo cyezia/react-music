0ce3d760378d4edb66cd8feba107332c9dda651b
cyezia <634940800@qq.com>
2022-06-28 23:40:55 +0800

    feat: 测试

--- src/application/Search/store/actionCreators.js
+++ src/application/Search/store/actionCreators.js
@@ -1,7 +1,7 @@
 import { SET_HOT_KEYWRODS, SET_SUGGEST_LIST, SET_RESULT_SONGS_LIST, SET_ENTER_LOADING } from './constants';
 import { fromJS } from "immutable";
 import { getHotKeyWordsRequest, getSuggestListRequest, getResultSongsListRequest } from '../../../api/request';
-import { type } from '@testing-library/user-event/dist/type';
+
 
 const changeHotKeyWords = (data) => ({
   type: SET_HOT_KEYWRODS,
